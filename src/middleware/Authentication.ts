b03c399bfe3c830d0e7826ee4eec4eee8226b25f
rross3 <rross@averoinc.com>
2020-03-19 12:42:44 -0600

    Prettier fixes

--- src/middleware/Authentication.ts
+++ src/middleware/Authentication.ts
@@ -3,7 +3,7 @@ import {ReqAttributes} from "../const/ReqAttibutes";
 
 export const authentication = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies[ReqAttributes.ACCESS_TOKEN];
-  console.log(token)
+  console.log(token);
   // if (!token) {
   //   res.status(401).send({message: "You must be logged in to use this service"});
   // }
