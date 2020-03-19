34ecb6338bbb120a695cf68b4851079806481d9a
rross3 <rross@averoinc.com>
2020-03-19 12:50:23 -0600

    Updating startup logic

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,50 @@
-import { server } from './server';
-
 import { ENV } from './env';
 import * as express from 'express';
 import * as cors from 'cors';
 import * as cookieParser from 'cookie-parser';
 import {authentication} from "./middleware/Authentication";
+import {gateway} from "./gateway";
+import {ApolloServer} from "apollo-server-express";
+import {ReqAttributes} from "./const/ReqAttibutes";
 
-const app = express();
-app.use(cors());
-app.use(cookieParser());
+(async () => {
+  const loadedGateway = await gateway.load();
+  const schema = loadedGateway.schema;
+  const executor = loadedGateway.executor;
 
-app.use('/healthy', async (req, res) => {
-  res.send({
-    message: 'Marketplace Gateway Healthy',
+  const server = new ApolloServer({
+    gateway,
+    schema,
+    executor,
+    subscriptions: false,
+    context: ({ req }) => {
+      const userInfo = req[ReqAttributes.USER_INFO];
+      return {userInfo};
+    },
   });
-});
 
-app.use(authentication);
+  const app = express();
+  app.use(cors());
+  app.use(cookieParser());
+  app.use(authentication);
 
-server.applyMiddleware({ app, path: '/graphql' });
 
-app.listen({ port: ENV.PORT }, () => {
-  console.log(`listening at :${ENV.PORT}...`);
+  app.use('/healthy', async (req, res) => {
+    res.send({
+      message: 'Marketplace Gateway up',
+    });
+  });
+
+  app.use('/', (req, res) => {
+    res.redirect('/graphql');
+  });
+
+
+  server.applyMiddleware({ app, path: '/graphql' });
+  app.listen({ port: ENV.PORT }, () => {
+    console.log(`listening at :${ENV.PORT}...`);
+  });
+})().catch(e => {
+  console.log(e);
+  process.exit(1);
 });
