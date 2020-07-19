f3bad60d8843f175a88527cd124ba0f98ae7171d
Reggie Ross <reggieross@reggies-mbp.lan>
2020-07-19 10:54:31 -0500

    Placing accesstoken in x-token header

--- src/AuthenticatedDateSource.ts
+++ src/AuthenticatedDateSource.ts
@@ -3,9 +3,6 @@ import { CookieMonster } from './client/CookieClient';
 
 export class AuthenticatedDataSource extends RemoteGraphQLDataSource {
   willSendRequest<Context>({ request, context }) {
-    request.http.headers.set(
-      'cookies',
-      CookieMonster.encodeCookie({ access_token: context.token })
-    );
+    request.http.headers.set('x-token', context.token);
   }
 }
